b50cb6d272ae9a5d87d8aa27b669d3836851e488
ilia24 <iliademertchian24@gmail.com>
2017-04-18 12:44:34 -0400

    Add migration for parts table

--- db/migrate/20170418164409_create_parts.rb
+++ db/migrate/20170418164409_create_parts.rb
@@ -1,8 +1,9 @@
 class CreateParts < ActiveRecord::Migration[5.0]
   def change
     create_table :parts do |t|
-
-      t.timestamps
+      t.string :name
+      t.string :manufacturer
+      t.decimal :cost
     end
   end
 end
